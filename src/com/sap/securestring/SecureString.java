9fdf0817b2f1b447e4dc3ece2a4714ea927503b1
Matthias Nott <matthias.nott@sap.com>
2014-07-30 16:28:57 +0200

    Add a very simple holder
    Just adding some holder here. We'll need to next get and also add some
    timer to it.
    
    Signed-off-by: Matthias Nott <matthias.nott@sap.com>

--- src/com/sap/securestring/SecureString.java
+++ src/com/sap/securestring/SecureString.java
@@ -1,5 +1,12 @@
 package com.sap.securestring;
 
 public class SecureString {
+  private char[] str;
+  
+  public SecureString() {}
+  
+  public SecureString(String str) {
+    this.str = str.toCharArray();
+  }
 
 }
